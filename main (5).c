19124a25f6e4dfeda150e4ef264bf395fd921975
sumeyya-arslan <89263842+sumeyya-arslan@users.noreply.github.com>
2024-05-20 14:20:41 +0300

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int sayi;
+  printf("sayi giriniz=");
+  scanf("%d",&sayi);
+  int kare=sayi*sayi;
+  int kup=sayi*sayi*sayi;
+  printf("küpü giriniz= %d\n",kup);
+   printf("kareyi giriniz= %d\n",kare); 
+   return 0;
+}
+
